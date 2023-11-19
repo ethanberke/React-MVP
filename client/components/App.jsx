3ec8854b01b87881c2b33765cf8bc0a4dab864fe
Ethan Berkebile <ethanberkebile@gmail.com>
2023-11-19 12:58:27 -0600

    Commit before large changes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -25,17 +25,41 @@ const App = () => {
   return (
     <main>
     <button onClick={handleRandomRecipe}>Random Recipe</button>
+
     <select value={selectedStyle} onChange={handleStyle}>
         <option value="">Select Style</option>
         <option value="American">American</option>
-        <option value="Mexican">Mexican</option></select>
+        <option value="Cambodian">Cambodian</option>
+        <option value="Dessert">Dessert</option>
+        <option value="Fusion">Fusion</option>
+        <option value="Italian">Italian</option>
+        <option value="Mexican">Mexican</option>
+        <option value="Polish">Polish</option>
+        <option value="Southern">Southern</option>
+        </select>
+
       <button onClick={handleContribute}>Contribute</button>
+
       {recipes.map((recipe) => (
         <div className="recipe" key={recipe.id}>
           <h2>{recipe.recipe_name}</h2>
           <p>Contributor: {recipe.contributor}</p>
         </div>
       ))}
+
+
+      <div>
+      <input type="text"
+      id="recipeName"
+      value="Recipe"/>
+      <input type="text"
+      id="contributorName"
+      value="Contribute"/>
+      <input type="text"
+      id="imageLink"
+      value="Image Link (optional)"/>
+      </div>
+
     </main>
   );
 };
